f4d6216c896f5f6f635c6fc89380c6f0da0a29a3
Esteban Miccio <esteban@bytewax.io>
2022-02-11 19:13:02 +0000

    Adding a loop to entrypoint.sh and updating package version in Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,6 @@
 
 cd $BYTEWAX_WORKDIR
 /venv/bin/python $BYTEWAX_PYTHON_FILE_PATH -w $BYTEWAX_WORKERS_PER_PROCESS -h $BYTEWAX_HOSTFILE_PATH -n $BYTEWAX_REPLICAS -p $(echo $BYTEWAX_POD_NAME | sed "s/$BYTEWAX_STATEFULSET_NAME-//g")
+
+echo 'Process ended. Keeping container alive...';
+while :; do sleep 1; done
