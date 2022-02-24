bd9184cc01822591419828a9dc1f2e45e2c6494c
Dan Herrera <whoahbot@bytewax.io>
2022-02-24 14:34:40 -0800

    Don't use __all__.

--- pysrc/bytewax/__init__.py
+++ pysrc/bytewax/__init__.py
@@ -1,13 +1,2 @@
-from .bytewax import Dataflow, cluster_main, sleep_keep_gil, sleep_release_gil
-
-from .execution import run, spawn_cluster, run_cluster
-
-__all__ = [
-    Dataflow,
-    cluster_main,
-    sleep_keep_gil,
-    sleep_release_gil,
-    run,
-    spawn_cluster,
-    run_cluster,
-]
+from .bytewax import *
+from .execution import *
