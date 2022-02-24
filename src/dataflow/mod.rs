2ce1e437cebccd1a9cd446df19e0b0bf527bd56c
Dan Herrera <whoahbot@bytewax.io>
2022-02-24 15:29:54 -0800

    Add dataflow and execution modules.

--- src/dataflow/mod.rs
+++ src/dataflow/mod.rs
@@ -12,9 +12,196 @@ use crate::operators::{
     capture, filter, flat_map, inspect, inspect_epoch, map, reduce, reduce_epoch,
     reduce_epoch_local, stateful_map,
 };
-use crate::{
-    build, hash, lift_2tuple, wrap_2tuple, Dataflow, Pump, TdPyAny, TdPyCallable, TdPyIterator,
-};
+
+use crate::{build, hash, lift_2tuple, wrap_2tuple, Pump, TdPyAny, TdPyCallable, TdPyIterator};
+
+/// The definition of Bytewax dataflow graph.
+///
+/// This isn't actually used during execution, just during building.
+///
+/// TODO: Right now this is just a linear dataflow only.
+#[pyclass(module = "bytewax")] // Required to support pickling.
+pub(crate) struct Dataflow {
+    steps: Vec<Step>,
+}
+
+#[pymethods]
+impl Dataflow {
+    #[new]
+    fn new() -> Self {
+        Self { steps: Vec::new() }
+    }
+
+    /// Pickle a Dataflow as a list of the steps.
+    fn __getstate__(&self, py: Python) -> PyResult<Py<PyAny>> {
+        Ok(self.steps.to_object(py))
+    }
+
+    /// Unpickle a Dataflow as a list of the steps.
+    fn __setstate__(&mut self, py: Python, state: Py<PyAny>) -> PyResult<()> {
+        self.steps = state.as_ref(py).extract()?;
+        Ok(())
+    }
+
+    /// **Map** is a one-to-one transformation of items.
+    ///
+    /// It calls a function `mapper(item: Any) => updated_item: Any`
+    /// on each item.
+    ///
+    /// It emits each transformed item downstream.
+    fn map(&mut self, mapper: TdPyCallable) {
+        self.steps.push(Step::Map { mapper });
+    }
+
+    /// **Flat Map** is a one-to-many transformation of items.
+    ///
+    /// It calls a function `mapper(item: Any) => emit: Iterable[Any]`
+    /// on each item.
+    ///
+    /// It emits each element in the downstream iterator individually.
+    fn flat_map(&mut self, mapper: TdPyCallable) {
+        self.steps.push(Step::FlatMap { mapper });
+    }
+
+    /// **Filter** selectively keeps only some items.
+    ///
+    /// It calls a function `predicate(item: Any) => should_emit:
+    /// bool` on each item.
+    ///
+    /// It emits the item downstream unmodified if the predicate
+    /// returns `True`.
+    fn filter(&mut self, predicate: TdPyCallable) {
+        self.steps.push(Step::Filter { predicate });
+    }
+
+    /// **Inspect** allows you to observe, but not modify, items.
+    ///
+    /// It calls a function `inspector(item: Any) => None` on each
+    /// item.
+    ///
+    /// The return value is ignored; it emits items downstream
+    /// unmodified.
+    fn inspect(&mut self, inspector: TdPyCallable) {
+        self.steps.push(Step::Inspect { inspector });
+    }
+
+    /// **Inspect Epoch** allows you to observe, but not modify, items
+    /// and their epochs.
+    ///
+    /// It calls a function `inspector(epoch: int, item: Any) => None`
+    /// on each item with its epoch.
+    ///
+    /// The return value is ignored; it emits items downstream
+    /// unmodified.
+    fn inspect_epoch(&mut self, inspector: TdPyCallable) {
+        self.steps.push(Step::InspectEpoch { inspector });
+    }
+
+    /// **Reduce** lets you combine items for a key into an
+    /// aggregator in epoch order.
+    ///
+    /// Since this is a stateful operator, it requires the the input
+    /// stream has items that are `(key, value)` tuples so we can
+    /// ensure that all relevant values are routed to the relevant
+    /// aggregator.
+    ///
+    /// It calls two functions:
+    ///
+    /// - A `reducer(aggregator: Any, value: Any) =>
+    /// updated_aggregator: Any` which combines two values. The
+    /// aggregator is initially the first value seen for a key. Values
+    /// will be passed in epoch order, but no order is defined within
+    /// an epoch.
+    ///
+    /// - An `is_complete(updated_aggregator: Any) => should_emit: bool` which
+    /// returns true if the most recent `(key, aggregator)` should be
+    /// emitted downstream and the aggregator for that key
+    /// forgotten. If there was only a single value for a key, it is
+    /// passed in as the aggregator here.
+    ///
+    /// It emits `(key, aggregator)` tuples downstream when you tell
+    /// it to.
+    fn reduce(&mut self, reducer: TdPyCallable, is_complete: TdPyCallable) {
+        self.steps.push(Step::Reduce {
+            reducer,
+            is_complete,
+        });
+    }
+
+    /// **Reduce Epoch** lets you combine all items for a key within
+    /// an epoch into an aggregator.
+    ///
+    /// This is like `reduce` but marks the aggregator as complete
+    /// automatically at the end of each epoch.
+    ///
+    /// Since this is a stateful operator, it requires the the input
+    /// stream has items that are `(key, value)` tuples so we can
+    /// ensure that all relevant values are routed to the relevant
+    /// aggregator.
+    ///
+    /// It calls a function `reducer(aggregator: Any, value: Any) =>
+    /// updated_aggregator: Any` which combines two values. The
+    /// aggregator is initially the first value seen for a key. Values
+    /// will be passed in arbitrary order.
+    ///
+    /// It emits `(key, aggregator)` tuples downstream at the end of
+    /// each epoch.
+    fn reduce_epoch(&mut self, reducer: TdPyCallable) {
+        self.steps.push(Step::ReduceEpoch { reducer });
+    }
+
+    /// **Reduce Epoch Local** lets you combine all items for a key
+    /// within an epoch _on a single worker._
+    ///
+    /// It is exactly like `reduce_epoch` but does no internal
+    /// exchange between workers. You'll probably should use that
+    /// instead unless you are using this as a network-overhead
+    /// optimization.
+    fn reduce_epoch_local(&mut self, reducer: TdPyCallable) {
+        self.steps.push(Step::ReduceEpochLocal { reducer });
+    }
+
+    /// **Stateful Map** is a one-to-one transformation of values in
+    /// `(key, value)` pairs, but allows you to reference a persistent
+    /// state for each key when doing the transformation.
+    ///
+    /// Since this is a stateful operator, it requires the the input
+    /// stream has items that are `(key, value)` tuples so we can
+    /// ensure that all relevant values are routed to the relevant
+    /// state.
+    ///
+    /// It calls two functions:
+    ///
+    /// - A `builder() => new_state: Any` which returns a new state
+    /// and will be called whenever a new key is encountered.
+    ///
+    /// - A `mapper(state: Any, value: Any) => (updated_state: Any,
+    /// updated_value: Any)` which transforms values. Values will be
+    /// passed in epoch order, but no order is defined within an
+    /// epoch. If the updated state is `None`, the state will be
+    /// forgotten.
+    ///
+    /// It emits a `(key, updated_value)` tuple downstream for each
+    /// input item.
+    fn stateful_map(&mut self, builder: TdPyCallable, mapper: TdPyCallable) {
+        self.steps.push(Step::StatefulMap { builder, mapper });
+    }
+
+    /// **Capture** causes all `(epoch, item)` tuples that pass by
+    /// this point in the Dataflow to be passed to the Dataflow's
+    /// output handler.
+    ///
+    /// Every dataflow must contain at least one capture.
+    ///
+    /// If you use this operator multiple times, the results will be
+    /// combined.
+    ///
+    /// There are no guarantees on the order that output is passed to
+    /// the handler. Read the attached epoch to discern order.
+    fn capture(&mut self) {
+        self.steps.push(Step::Capture {});
+    }
+}
 
 /// The definition of one step in a Bytewax dataflow graph.
 ///
@@ -252,4 +439,9 @@ where
             Err("Dataflow needs to contain at least one capture".into())
         }
     })
+}
+
+pub(crate) fn register(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Dataflow>()?;
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-#![allow(non_snake_case)]
-extern crate rand;
 #[macro_use(defer)]
 extern crate scopeguard;
 
-use pyo3::exceptions::{PyRuntimeError, PyTypeError, PyValueError};
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 use pyo3::types::*;
 use serde::ser::Error;
@@ -12,20 +10,15 @@ use std::fmt;
 use std::hash::Hash;
 use std::hash::Hasher;
 use std::ops::Deref;
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::AtomicUsize;
-use std::sync::atomic::Ordering;
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 use timely::dataflow::*;
 
 pub(crate) mod dataflow;
+pub(crate) mod execution;
 pub(crate) mod operators;
 pub(crate) mod webserver;
-
-use crate::dataflow::*;
 
 #[macro_use]
 pub(crate) mod macros;
@@ -298,194 +291,6 @@ impl TdPyCallable {
     }
 }
 
-/// The definition of Bytewax dataflow graph.
-///
-/// This isn't actually used during execution, just during building.
-///
-/// TODO: Right now this is just a linear dataflow only.
-#[pyclass(module = "bytewax")] // Required to support pickling.
-struct Dataflow {
-    steps: Vec<Step>,
-}
-
-#[pymethods]
-impl Dataflow {
-    #[new]
-    fn new() -> Self {
-        Self { steps: Vec::new() }
-    }
-
-    /// Pickle a Dataflow as a list of the steps.
-    fn __getstate__(&self, py: Python) -> PyResult<Py<PyAny>> {
-        Ok(self.steps.to_object(py))
-    }
-
-    /// Unpickle a Dataflow as a list of the steps.
-    fn __setstate__(&mut self, py: Python, state: Py<PyAny>) -> PyResult<()> {
-        self.steps = state.as_ref(py).extract()?;
-        Ok(())
-    }
-
-    /// **Map** is a one-to-one transformation of items.
-    ///
-    /// It calls a function `mapper(item: Any) => updated_item: Any`
-    /// on each item.
-    ///
-    /// It emits each transformed item downstream.
-    fn map(&mut self, mapper: TdPyCallable) {
-        self.steps.push(Step::Map { mapper });
-    }
-
-    /// **Flat Map** is a one-to-many transformation of items.
-    ///
-    /// It calls a function `mapper(item: Any) => emit: Iterable[Any]`
-    /// on each item.
-    ///
-    /// It emits each element in the downstream iterator individually.
-    fn flat_map(&mut self, mapper: TdPyCallable) {
-        self.steps.push(Step::FlatMap { mapper });
-    }
-
-    /// **Filter** selectively keeps only some items.
-    ///
-    /// It calls a function `predicate(item: Any) => should_emit:
-    /// bool` on each item.
-    ///
-    /// It emits the item downstream unmodified if the predicate
-    /// returns `True`.
-    fn filter(&mut self, predicate: TdPyCallable) {
-        self.steps.push(Step::Filter { predicate });
-    }
-
-    /// **Inspect** allows you to observe, but not modify, items.
-    ///
-    /// It calls a function `inspector(item: Any) => None` on each
-    /// item.
-    ///
-    /// The return value is ignored; it emits items downstream
-    /// unmodified.
-    fn inspect(&mut self, inspector: TdPyCallable) {
-        self.steps.push(Step::Inspect { inspector });
-    }
-
-    /// **Inspect Epoch** allows you to observe, but not modify, items
-    /// and their epochs.
-    ///
-    /// It calls a function `inspector(epoch: int, item: Any) => None`
-    /// on each item with its epoch.
-    ///
-    /// The return value is ignored; it emits items downstream
-    /// unmodified.
-    fn inspect_epoch(&mut self, inspector: TdPyCallable) {
-        self.steps.push(Step::InspectEpoch { inspector });
-    }
-
-    /// **Reduce** lets you combine items for a key into an
-    /// aggregator in epoch order.
-    ///
-    /// Since this is a stateful operator, it requires the the input
-    /// stream has items that are `(key, value)` tuples so we can
-    /// ensure that all relevant values are routed to the relevant
-    /// aggregator.
-    ///
-    /// It calls two functions:
-    ///
-    /// - A `reducer(aggregator: Any, value: Any) =>
-    /// updated_aggregator: Any` which combines two values. The
-    /// aggregator is initially the first value seen for a key. Values
-    /// will be passed in epoch order, but no order is defined within
-    /// an epoch.
-    ///
-    /// - An `is_complete(updated_aggregator: Any) => should_emit: bool` which
-    /// returns true if the most recent `(key, aggregator)` should be
-    /// emitted downstream and the aggregator for that key
-    /// forgotten. If there was only a single value for a key, it is
-    /// passed in as the aggregator here.
-    ///
-    /// It emits `(key, aggregator)` tuples downstream when you tell
-    /// it to.
-    fn reduce(&mut self, reducer: TdPyCallable, is_complete: TdPyCallable) {
-        self.steps.push(Step::Reduce {
-            reducer,
-            is_complete,
-        });
-    }
-
-    /// **Reduce Epoch** lets you combine all items for a key within
-    /// an epoch into an aggregator.
-    ///
-    /// This is like `reduce` but marks the aggregator as complete
-    /// automatically at the end of each epoch.
-    ///
-    /// Since this is a stateful operator, it requires the the input
-    /// stream has items that are `(key, value)` tuples so we can
-    /// ensure that all relevant values are routed to the relevant
-    /// aggregator.
-    ///
-    /// It calls a function `reducer(aggregator: Any, value: Any) =>
-    /// updated_aggregator: Any` which combines two values. The
-    /// aggregator is initially the first value seen for a key. Values
-    /// will be passed in arbitrary order.
-    ///
-    /// It emits `(key, aggregator)` tuples downstream at the end of
-    /// each epoch.
-    fn reduce_epoch(&mut self, reducer: TdPyCallable) {
-        self.steps.push(Step::ReduceEpoch { reducer });
-    }
-
-    /// **Reduce Epoch Local** lets you combine all items for a key
-    /// within an epoch _on a single worker._
-    ///
-    /// It is exactly like `reduce_epoch` but does no internal
-    /// exchange between workers. You'll probably should use that
-    /// instead unless you are using this as a network-overhead
-    /// optimization.
-    fn reduce_epoch_local(&mut self, reducer: TdPyCallable) {
-        self.steps.push(Step::ReduceEpochLocal { reducer });
-    }
-
-    /// **Stateful Map** is a one-to-one transformation of values in
-    /// `(key, value)` pairs, but allows you to reference a persistent
-    /// state for each key when doing the transformation.
-    ///
-    /// Since this is a stateful operator, it requires the the input
-    /// stream has items that are `(key, value)` tuples so we can
-    /// ensure that all relevant values are routed to the relevant
-    /// state.
-    ///
-    /// It calls two functions:
-    ///
-    /// - A `builder() => new_state: Any` which returns a new state
-    /// and will be called whenever a new key is encountered.
-    ///
-    /// - A `mapper(state: Any, value: Any) => (updated_state: Any,
-    /// updated_value: Any)` which transforms values. Values will be
-    /// passed in epoch order, but no order is defined within an
-    /// epoch. If the updated state is `None`, the state will be
-    /// forgotten.
-    ///
-    /// It emits a `(key, updated_value)` tuple downstream for each
-    /// input item.
-    fn stateful_map(&mut self, builder: TdPyCallable, mapper: TdPyCallable) {
-        self.steps.push(Step::StatefulMap { builder, mapper });
-    }
-
-    /// **Capture** causes all `(epoch, item)` tuples that pass by
-    /// this point in the Dataflow to be passed to the Dataflow's
-    /// output handler.
-    ///
-    /// Every dataflow must contain at least one capture.
-    ///
-    /// If you use this operator multiple times, the results will be
-    /// combined.
-    ///
-    /// There are no guarantees on the order that output is passed to
-    /// the handler. Read the attached epoch to discern order.
-    fn capture(&mut self) {
-        self.steps.push(Step::Capture {});
-    }
-}
-
 fn build(builder: &TdPyCallable) -> TdPyAny {
     Python::with_gil(|py| with_traceback!(py, builder.call0(py)).into())
 }
@@ -503,11 +308,24 @@ fn hash(key: &TdPyAny) -> u64 {
     key.hash(&mut hasher);
     hasher.finish()
 }
+
+#[pyfunction]
+fn sleep_keep_gil(secs: u64) {
+    thread::sleep(Duration::from_secs(secs));
+}
+
+#[pyfunction]
+fn sleep_release_gil(py: Python, secs: u64) {
+    py.allow_threads(|| {
+        thread::sleep(Duration::from_secs(secs));
+    });
+}
+
 /// Encapsulates the process of pulling data out of the input Python
 /// iterator and feeding it into Timely.
 ///
 /// This will be called in the worker's "main" loop to feed data in.
-struct Pump {
+pub(crate) struct Pump {
     pull_from_pyiter: TdPyIterator,
     pyiter_is_empty: bool,
     push_to_timely: InputHandle<u64, TdPyAny>,
@@ -542,265 +360,11 @@ impl Pump {
     }
 }
 
-/// "Main loop" of a Timely worker thread.
-///
-/// 1. Pump [`Pump`]s (get input data from Python into Timely).
-///
-/// 2. Dispose of empty [`Pump`]s and Probes which indicate there's no
-/// data left to process in that dataflow.
-///
-/// 3. Call [timely::worker::Worker::step] to tell Timely to do
-/// whatever work it can.
-fn worker_main<A>(
-    mut pumps_with_input_remaining: Vec<Pump>,
-    mut probes_with_timestamps_inflight: Vec<ProbeHandle<u64>>,
-    interrupt_flag: &AtomicBool,
-    worker: &mut timely::worker::Worker<A>,
-) where
-    A: timely::communication::Allocate,
-{
-    while (!pumps_with_input_remaining.is_empty() || !probes_with_timestamps_inflight.is_empty())
-        && !interrupt_flag.load(Ordering::Relaxed)
-    {
-        if !pumps_with_input_remaining.is_empty() {
-            let mut updated_pumps = Vec::new();
-            for mut pump in pumps_with_input_remaining.into_iter() {
-                pump.pump();
-                if pump.input_remains() {
-                    updated_pumps.push(pump);
-                }
-            }
-            pumps_with_input_remaining = updated_pumps;
-        }
-        if !probes_with_timestamps_inflight.is_empty() {
-            let mut updated_probes = Vec::new();
-            for probe in probes_with_timestamps_inflight.into_iter() {
-                if !probe.done() {
-                    updated_probes.push(probe);
-                }
-            }
-            probes_with_timestamps_inflight = updated_probes;
-        }
-
-        worker.step();
-    }
-}
-
-fn shutdown_worker<A>(worker: &mut timely::worker::Worker<A>)
-where
-    A: timely::communication::Allocate,
-{
-    for dataflow_id in worker.installed_dataflows() {
-        worker.drop_dataflow(dataflow_id);
-    }
-}
-
-/// Private shim for `run()` but takes builder functions so we can
-/// re-use `build_dataflow()`.
-#[pyfunction]
-fn _run(
-    py: Python,
-    flow: Py<Dataflow>,
-    input_builder: TdPyCallable,
-    output_builder: TdPyCallable,
-) -> PyResult<()> {
-    let result = py.allow_threads(move || {
-        std::panic::catch_unwind(|| {
-            // TODO: See if we can PR Timely to not cast result error
-            // to a String. Then we could "raise" Python errors from
-            // the builder directly. Probably also as part of the
-            // panic recast issue below.
-            timely::execute::execute_directly::<Result<(), String>, _>(move |worker| {
-                let (pump, probe) = Python::with_gil(|py| {
-                    let flow = &flow.as_ref(py).borrow();
-                    build_dataflow(worker, py, flow, input_builder, output_builder)
-                })?;
-
-                worker_main(vec![pump], vec![probe], &AtomicBool::new(false), worker);
-
-                shutdown_worker(worker);
-
-                Ok(())
-            })
-        })
-    });
-
-    match result {
-        Ok(Ok(ok)) => Ok(ok),
-        Ok(Err(build_err_str)) => Err(PyValueError::new_err(build_err_str)),
-        Err(panic_err) => {
-            let pyerr = if let Some(pyerr) = panic_err.downcast_ref::<PyErr>() {
-                pyerr.clone_ref(py)
-            } else {
-                PyRuntimeError::new_err("Panic in Rust code")
-            };
-            Err(pyerr)
-        }
-    }
-}
-
-/// Execute a dataflow in the current process as part of a cluster.
-///
-/// You have to coordinate starting up all the processes in the
-/// cluster and ensuring they each are assigned a unique ID and know
-/// the addresses of other processes. You'd commonly use this for
-/// starting processes as part of a Kubernetes cluster.
-///
-/// Blocks until execution is complete.
-///
-/// See `run_cluster()` for a convenience method to pass data through
-/// a dataflow for notebook development.
-///
-/// See `spawn_cluster()` for starting a simple cluster locally on one
-/// machine.
-///
-/// >>> flow = Dataflow()
-/// >>> def input_builder(worker_index, worker_count):
-/// ...     return enumerate(range(3))
-/// >>> def output_builder(worker_index, worker_count):
-/// ...     return print
-/// >>> cluster_main(flow, input_builder, output_builder)
-///
-/// Args:
-///     flow: Dataflow to run.
-///     input_builder: Returns input that each worker thread should
-///         process.
-///     output_builder: Returns a callback function for each worker
-///         thread, called with `(epoch, item)` whenever and item
-///         passes by a capture operator on this process.
-///     addresses: List of host/port addresses for all processes in
-///         this cluster (including this one).
-///     proc_id: Index of this process in cluster; starts from 0.
-#[pyfunction]
-fn cluster_main(
-    py: Python,
-    flow: Py<Dataflow>,
-    input_builder: TdPyCallable,
-    output_builder: TdPyCallable,
-    addresses: Option<Vec<String>>,
-    proc_id: usize,
-    worker_count_per_proc: usize,
-) -> PyResult<()> {
-    py.allow_threads(move || {
-        let addresses = addresses.unwrap_or_default();
-        let (builders, other) = if addresses.len() < 1 {
-            timely::CommunicationConfig::Process(worker_count_per_proc)
-        } else {
-            timely::CommunicationConfig::Cluster {
-                threads: worker_count_per_proc,
-                process: proc_id,
-                addresses,
-                report: false,
-                log_fn: Box::new(|_| None),
-            }
-        }
-        .try_build()
-        .map_err(PyRuntimeError::new_err)?;
-
-        let should_shutdown = Arc::new(AtomicBool::new(false));
-        let should_shutdown_w = should_shutdown.clone();
-        let should_shutdown_p = should_shutdown.clone();
-        // We can drop these if we want to use nightly
-        // Thread::is_running
-        // https://github.com/rust-lang/rust/issues/90470
-        let shutdown_worker_count = Arc::new(AtomicUsize::new(0));
-        let shutdown_worker_count_w = shutdown_worker_count.clone();
-
-        // Panic hook is per-process, so this isn't perfect as you
-        // can't call Executor.build_and_run() concurrently.
-        let default_hook = std::panic::take_hook();
-        std::panic::set_hook(Box::new(move |info| {
-            should_shutdown_p.store(true, Ordering::Relaxed);
-
-            if let Some(pyerr) = info.payload().downcast_ref::<PyErr>() {
-                Python::with_gil(|py| pyerr.print(py));
-            } else {
-                default_hook(info);
-            }
-        }));
-        // Don't chain panic hooks if we run multiple
-        // dataflows. Really this is all a hack because the panic
-        // hook is global state. There's some talk of per-thread
-        // panic hooks which would help
-        // here. https://internals.rust-lang.org/t/pre-rfc-should-std-set-hook-have-a-per-thread-version/9518/3
-        defer! {
-            let _ = std::panic::take_hook();
-        }
-
-        let guards = timely::execute::execute_from::<_, Result<(), String>, _>(
-            builders,
-            other,
-            timely::WorkerConfig::default(),
-            move |worker| {
-                defer! {
-                    shutdown_worker_count_w.fetch_add(1, Ordering::Relaxed);
-                }
-
-                let (pump, probe) = Python::with_gil(|py| {
-                    let flow = &flow.as_ref(py).borrow();
-                    let input_builder = input_builder.clone_ref(py);
-                    let output_builder = output_builder.clone_ref(py);
-                    build_dataflow(worker, py, flow, input_builder, output_builder)
-                })?;
-
-                worker_main(vec![pump], vec![probe], &should_shutdown_w, worker);
-
-                shutdown_worker(worker);
-
-                Ok(())
-            },
-        )
-        .map_err(PyRuntimeError::new_err)?;
-
-        // Recreating what Python does in Thread.join() to "block"
-        // but also check interrupt handlers.
-        // https://github.com/python/cpython/blob/204946986feee7bc80b233350377d24d20fcb1b8/Modules/_threadmodule.c#L81
-        let workers_in_proc_count = guards.guards().len();
-        while shutdown_worker_count.load(Ordering::Relaxed) < workers_in_proc_count {
-            thread::sleep(Duration::from_millis(1));
-            Python::with_gil(|py| Python::check_signals(py)).map_err(|err| {
-                should_shutdown.store(true, Ordering::Relaxed);
-                err
-            })?;
-        }
-        for maybe_worker_panic in guards.join() {
-            // TODO: See if we can PR Timely to not cast panic info to
-            // String. Then we could re-raise Python exception in main
-            // thread and not need to print in panic::set_hook above,
-            // although we still need it to tell the other workers to
-            // do graceful shutdown.
-            match maybe_worker_panic {
-                Ok(Ok(ok)) => Ok(ok),
-                Ok(Err(build_err_str)) => Err(PyValueError::new_err(build_err_str)),
-                Err(_panic_err) => Err(PyRuntimeError::new_err(
-                    "Worker thread died; look for errors above",
-                )),
-            }?;
-        }
-
-        Ok(())
-    })
-}
-
-#[pyfunction]
-fn sleep_keep_gil(secs: u64) {
-    thread::sleep(Duration::from_secs(secs));
-}
-
-#[pyfunction]
-fn sleep_release_gil(py: Python, secs: u64) {
-    py.allow_threads(|| {
-        thread::sleep(Duration::from_secs(secs));
-    });
-}
-
 #[pymodule]
 #[pyo3(name = "bytewax")]
 fn mod_tiny_dancer(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Dataflow>()?;
-
-    m.add_function(wrap_pyfunction!(_run, m)?)?;
-    m.add_function(wrap_pyfunction!(cluster_main, m)?)?;
+    execution::register(_py, m)?;
+    dataflow::register(_py, m)?;
 
     m.add_function(wrap_pyfunction!(sleep_keep_gil, m)?)?;
     m.add_function(wrap_pyfunction!(sleep_release_gil, m)?)?;
