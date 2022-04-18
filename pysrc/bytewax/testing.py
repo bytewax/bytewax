23c1c5c4d0da993b92020becb186b779ea3dc637
David Selassie <david@bytewax.io>
2022-04-18 12:02:34 -0700

    `testing` module
    Adds `bytewax.testing` as a module in the library which contains
    helper tools for end users testing Bytewax dataflows.
    
    The two things in there to start facilitate doctests:
    
    1. `test_print()`: Have you ever tried printing dataflow output to
    stdout but with multiple worker? Sometimes it is all interleaved and
    that would make the output difficult to test. This wraps `print()` in
    a mutex, so only one worker will print at a time. This allows for
    deterministic output testing.
    
    2. `doctest_ctx`: Doctests and spawning multiprocessing don't play
    well together becuase there's no "main" Python file for the subprocess
    to import to build back up state. This lets you specify a
    `multiprocessing` "context" which doesn't actually spawn a pool of
    subprocesses to do our local cluster work; instead everything happens
    in process but in background threads. You can pass this to
    `spawn_cluster()` or `run_cluster()`. Yes, this isn't "correct"
    behavior, but the point of doctests is to demonstrate the API, and the
    API and coarse behavior is unchanged. We still have proper pytests to
    really test the true cluster behavior.

---
+++ pysrc/bytewax/testing.py
@@ -0,0 +1,52 @@
+"""Helper tools for testing dataflows.
+"""
+import multiprocessing.dummy
+from contextlib import contextmanager
+from threading import Lock
+
+
+_print_lock = Lock()
+
+
+def test_print(*args, **kwargs):
+    """A version of `print()` which takes an in-process lock to prevent
+    multiple worker threads from writing simultaneously which results
+    in interleaved output.
+
+    You'd use this if you're integration testing a dataflow and want
+    more deterministic output. Remember that even with this, the items
+    from multi-worker output might be "out-of-order" because each
+    worker is racing each other. You probably want to sort your output
+    in some way.
+
+    Arguments are passed through to `print()` unmodified.
+
+    """
+    with _print_lock:
+        print(*args, flush=True, **kwargs)
+
+
+doctest_ctx = multiprocessing.dummy
+"""Use this `multiprocessing` context when running in doctests.
+
+Pass to `bytewax.spawn_cluster()` and `bytewax.run_cluster()`.
+
+Spawning subprocesses is fraught in doctest contexts, so use this to
+demonstrate the API works, but not actually run via multiple
+processes. We have other normal `pytest` tests which actually test
+behavior. Don't worry.
+
+"""
+
+
+@contextmanager
+def _Manager():
+    """`multiprocessing.dummy.Manager()` doesn't support being a context
+    manager like a real `multiprocessing.Manager()` does... So let's
+    monkey patch it.
+
+    """
+    yield doctest_ctx
+
+
+doctest_ctx.Manager = _Manager
