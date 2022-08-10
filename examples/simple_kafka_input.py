35a236ec855359dbe7d98841ddc09de5d3338f22
Federico Dolce <psykopear@gmail.com>
2022-08-10 11:46:50 -0700

    Fixed simple_kafka_input

--- examples/simple_kafka_input.py
+++ examples/simple_kafka_input.py
@@ -1,9 +1,10 @@
 import json
 
 from bytewax.dataflow import Dataflow
-from bytewax.execution import run_main
 from bytewax.inputs import KafkaInputConfig
 from bytewax.outputs import StdOutputConfig
+from bytewax.execution import spawn_cluster
+from bytewax.outputs import ManualEpochOutputConfig
 
 flow = Dataflow()
 
@@ -25,8 +26,27 @@ def deserialize(key_bytes__payload_bytes):
 
 flow.map(deserialize)
 
+
+def filter_invalid_json_err(item):
+    return item
+
+
+def output_builder(worker_index, worker_count):
+    def output_handler(epoch_item):
+        epoch, (key, payload) = epoch_item
+        print(epoch, json.dumps(key), json.dumps(payload))
+    return output_handler
+
+
 flow.capture(StdOutputConfig())
 
 
 if __name__ == "__main__":
-    run_main(flow)
+    input_config = KafkaInputConfig(
+        "localhost:9092", "example_group_id", "drivers"
+    )
+    flow = Dataflow(input_config)
+    flow.map(deserialize)
+    flow.flat_map(filter_invalid_json_err)
+    flow.capture(ManualEpochOutputConfig(output_builder))
+    spawn_cluster(flow)
