d79e960a5aa3ffccc581d8028c9f51037b782235
Federico Dolce <psykopear@gmail.com>
2022-08-22 17:17:09 +0200

    Fixed examples/event_to_parquet.py

--- examples/events_to_parquet.py
+++ examples/events_to_parquet.py
@@ -1,34 +1,36 @@
-import datetime
 import json
-import time
-
 import pandas
-import pyarrow.parquet as parquet
+
 from pandas import DataFrame
-from pyarrow import Table
+from pyarrow import Table, parquet
 from utils import fake_events
+from datetime import timedelta, datetime
 
-from bytewax import Dataflow, inputs, parse, spawn_cluster
+from bytewax.dataflow import Dataflow
+from bytewax.execution import spawn_cluster
 from bytewax.inputs import ManualInputConfig
+from bytewax.outputs import ManualOutputConfig
+from bytewax.window import TumblingWindowConfig, SystemClockConfig
 
 
-# Collect 5 second tumbling windows of data and write them out as
-# Parquet datasets. `fake_events` will generate events for multiple
+# `fake_events` will generate events for multiple
 # days around today. Each worker will generate independent fake
 # events.
-@inputs.yield_epochs
 def input_builder(worker_index, worker_count, resume_epoch):
-    return inputs.tumbling_epoch(
-        fake_events.generate_web_events(), datetime.timedelta(seconds=5)
-    )
+    state = None
+
+    def generator():
+        for event in fake_events.generate_web_events():
+            yield state, event
+
+    return generator()
 
 
 # Arrow assigns a UUID to each worker / window's file so they won't
 # clobber each other. They are further automatically placed in the
 # correct directory structure based on date and path.
-def write_parquet(epoch__events_df):
+def write_parquet(events_df):
     """Write events as partitioned Parquet in `$PWD/parquet_demo_out/`"""
-    epoch, events_df = epoch__events_df
     table = Table.from_pandas(events_df)
     parquet.write_to_dataset(
         table,
@@ -44,7 +46,7 @@ def output_builder(worker_index, worker_count):
 
 
 def add_date_columns(event):
-    timestamp = datetime.datetime.fromisoformat(event["event_timestamp"])
+    timestamp = datetime.fromisoformat(event["event_timestamp"])
     event["year"] = timestamp.year
     event["month"] = timestamp.month
     event["day"] = timestamp.day
@@ -64,21 +66,24 @@ def drop_page(page__events_df):
     return events_df
 
 
+cc = SystemClockConfig()
+wc = TumblingWindowConfig(length=timedelta(seconds=5))
+
 flow = Dataflow()
+flow.input("input", ManualInputConfig(input_builder))
 flow.map(json.loads)
 # {"page_url_path": "/path", "event_timestamp": "2022-01-02 03:04:05", ...}
 flow.map(add_date_columns)
 # {"page_url_path": "/path", "year": 2022, "month": 1, "day": 5, ... }
 flow.map(group_by_page)
 # ("/path", DataFrame([{"page_url_path": "/path", "year": 2022, "month": 1, "day": 5, ... }]))
-flow.reduce_epoch_local(append_event)
+flow.reduce_window("reducer", cc, wc, append_event)
 # ("/path", DataFrame([{"page_url_path": "/path", ...}, ...]))
 flow.map(drop_page)
 # DataFrame([{"page_url_path": "/path", ...}, ...])
-flow.capture()
+flow.capture(ManualOutputConfig(output_builder))
 
 
 if __name__ == "__main__":
-    spawn_cluster(
-        flow, ManualInputConfig(input_builder), output_builder, **parse.cluster_args()
-    )
+    # run_main(flow)
+    spawn_cluster(flow)
