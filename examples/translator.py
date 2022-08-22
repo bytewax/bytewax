522a5f25451fdaa8320fcc6fe8db42e96a55d34e
Blake Johnson <blake@bytewax.io>
2022-08-22 15:12:56 -0700

    Updates translator

--- examples/translator.py
+++ examples/translator.py
@@ -1,7 +1,15 @@
 from transformers import pipeline
 
-import bytewax
-from bytewax import Dataflow, inputs, parse, run_cluster
+from bytewax.dataflow import Dataflow
+from bytewax.execution import run_main
+from bytewax.inputs import ManualInputConfig
+from bytewax.outputs import StdOutputConfig
+
+
+def input_builder(worker_index, worker_count, resume_state):
+    state = None  # ignore recovery
+    for line in open("examples/sample_data/lyrics.txt"):
+        yield state, line
 
 
 def build_predict(translator):
@@ -12,22 +20,19 @@ def build_predict(translator):
     return predict
 
 
-def inspector(en_de):
+def format_output(en_de):
     en, de = en_de
-    print(f"{en} -> {de}")
+    return f"{en} -> {de}"
 
+
+translator = pipeline("translation_en_to_de")
+
+flow = Dataflow()
+flow.input("input", ManualInputConfig(input_builder))
+flow.map(str.strip)
+flow.map(build_predict(translator))
+flow.map(format_output)
+flow.capture(StdOutputConfig())
 
 if __name__ == "__main__":
-    translator = pipeline("translation_en_to_de")
-
-    flow = Dataflow()
-    flow.map(str.strip)
-    flow.map(build_predict(translator))
-    flow.capture()
-
-    for epoch, item in run_cluster(
-        flow,
-        inputs.single_batch(open("examples/sample_data/lyrics.txt")),
-        **parse.cluster_args(),
-    ):
-        inspector(item)
+    run_main(flow)
