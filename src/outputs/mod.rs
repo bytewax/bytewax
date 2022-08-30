b8ade42b5059ad49f30cb583fc7c8f38b22fcc67
Blake Johnson <blake@bytewax.io>
2022-08-30 13:56:17 -0700

    Initial working KafkaOutput

--- src/outputs/mod.rs
+++ src/outputs/mod.rs
@@ -21,8 +21,14 @@ use crate::{
     pyo3_extensions::{TdPyAny, TdPyCallable},
     unwrap_any, StringResult,
 };
-use pyo3::ffi::PySys_WriteStdout;
 use pyo3::{exceptions::PyValueError, prelude::*};
+use pyo3::ffi::PySys_WriteStdout;
+use rdkafka::{
+    producer::{BaseProducer, BaseRecord, Producer},
+    ClientConfig,
+};
+use send_wrapper::SendWrapper;
+use std::time::Duration;
 
 /// Base class for an output config.
 ///
@@ -172,6 +178,68 @@ impl ManualEpochOutputConfig {
     }
 }
 
+/// Use [Kafka](https://kafka.apache.org) as the output.
+///
+/// A `capture` using KafkaOutput expects to receive data
+/// structured as two-tuples of (key, payload) to form a Kafka
+/// record.
+///
+/// Args:
+///
+///   brokers (List[str]): List of `host:port` strings of Kafka
+///       brokers.
+///
+///   topic (str): Topic to which producer will send records.
+///
+/// Returns:
+///
+///   Config object. Pass this as the `output_config` argument to the
+///   `bytewax.dataflow.Dataflow.output`.
+#[pyclass(module = "bytewax.outputs", extends = OutputConfig)]
+#[pyo3(text_signature = "(brokers, topic)")]
+pub(crate) struct KafkaOutputConfig {
+    #[pyo3(get)]
+    brokers: Vec<String>,
+    #[pyo3(get)]
+    topic: String,
+}
+
+#[pymethods]
+impl KafkaOutputConfig {
+    #[new]
+    #[args(brokers, topic)]
+    fn new(brokers: Vec<String>, topic: String) -> (Self, OutputConfig) {
+        (Self { brokers, topic }, OutputConfig {})
+    }
+
+    fn __getstate__(&self) -> (&str, Vec<String>, String) {
+        (
+            "KafkaOutputConfig",
+            self.brokers.clone(),
+            self.topic.clone(),
+        )
+    }
+
+    /// Egregious hack see [`SqliteRecoveryConfig::__getnewargs__`].
+    fn __getnewargs__(&self) -> (Vec<String>, &str) {
+        let s = "UNINIT_PICKLED_STRING";
+        (Vec::new(), s)
+    }
+
+    /// Unpickle from tuple of arguments.
+    fn __setstate__(&mut self, state: &PyAny) -> PyResult<()> {
+        if let Ok(("KafkaOutputConfig", brokers, topic)) = state.extract() {
+            self.brokers = brokers;
+            self.topic = topic;
+            Ok(())
+        } else {
+            Err(PyValueError::new_err(format!(
+                "bad pickle contents for KafkaOutputConfig: {state:?}"
+            )))
+        }
+    }
+}
+
 /// Write the output items to standard out.
 ///
 /// Items must have a valid `__str__`. If not, map the items into a
@@ -252,6 +320,16 @@ pub(crate) fn build_output_writer(
         let writer = py.allow_threads(StdOutput::new);
 
         Ok(Box::new(writer))
+    } else if let Ok(config) = config.downcast::<PyCell<KafkaOutputConfig>>() {
+        let config = config.borrow();
+
+        let brokers = &config.brokers;
+        let topic = &config.topic;
+
+        let writer =
+            py.allow_threads(|| SendWrapper::new(KafkaOutput::new(brokers, topic.to_string())));
+
+        Ok(Box::new(writer.take()))
     } else {
         let pytype = config.get_type();
         Err(format!("Unknown output_config type: {pytype}"))
@@ -343,6 +421,43 @@ impl OutputWriter<u64, TdPyAny> for StdOutput {
     }
 }
 
+/// Produce output to kafka stream
+struct KafkaOutput {
+    producer: BaseProducer,
+    topic: String,
+}
+
+impl KafkaOutput {
+    fn new(brokers: &[String], topic: String) -> Self {
+        let producer: BaseProducer = ClientConfig::new()
+            .set("bootstrap.servers", brokers.join(","))
+            .create()
+            .expect("Producer creation error");
+
+        Self { producer, topic }
+    }
+}
+
+impl OutputWriter<u64, TdPyAny> for KafkaOutput {
+    fn push(&mut self, _epoch: u64, item: TdPyAny) {
+        let is = item.to_string();
+        self.producer
+            .send(
+                BaseRecord::to(&self.topic)
+                    .payload(&is)
+                    // errr what's the key
+                    .key("and this is a key"),
+            )
+            .expect("Failed to enqueue");
+
+        // Poll to process all the asynchronous delivery events.
+        self.producer.poll(Duration::from_millis(0));
+
+        // And/or flush the producer before dropping it.
+        self.producer.flush(Duration::from_secs(1));
+    }
+}
+
 pub(crate) fn capture(
     writer: &mut Box<dyn OutputWriter<u64, TdPyAny>>,
     epoch: &u64,
@@ -356,5 +471,6 @@ pub(crate) fn register(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<ManualOutputConfig>()?;
     m.add_class::<ManualEpochOutputConfig>()?;
     m.add_class::<StdOutputConfig>()?;
+    m.add_class::<KafkaOutputConfig>()?;
     Ok(())
 }
