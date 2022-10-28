8bdf1d1ee2bfd1fc7e14285a82de19f465267d53
Federico Dolce <psykopear@gmail.com>
2022-10-28 15:11:28 +0200

    Removed unused comment

--- pdocs.sh
+++ pdocs.sh
@@ -4,5 +4,4 @@ docker build -f apidocs/Dockerfile -t bytewax-pdocs .
 rm -rf apidocs/html
 mkdir apidocs/html
 # Run the image, the entrypoint will do the rest
-# docker run --rm --mount type=bind,source="$(pwd)"/apidocs/html,target=/bytewax/apidocs/html
 docker run --rm -v "$(pwd)":/bytewax bytewax-pdocs:latest
