17676c4cdddf1a8df66eeef76598514b4bc97a96
David Selassie <dsel@fastmail.com>
2023-11-02 13:02:54 -0700

    Breaks out unary test logic

--- pytests/operators/test_unary.py
+++ pytests/operators/test_unary.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Any, Iterable, Optional, Tuple
+from typing import Any, List, Optional, Tuple
 
 from bytewax.dataflow import Dataflow
 from bytewax.operators import UnaryLogic
@@ -8,30 +8,47 @@ from bytewax.testing import TestingSink, TestingSource, run_main
 ZERO_TD = timedelta(seconds=0)
 
 
+class BaseTestLogic(UnaryLogic):
+    item_triggers_notify = False
+    after_item = UnaryLogic.RETAIN
+    after_notify = UnaryLogic.RETAIN
+    after_eof = UnaryLogic.RETAIN
+
+    def __init__(self, _now: datetime, state: Any):
+        self._notify_at: Optional[datetime] = None
+        self._state = state if state is not None else "NEW"
+
+    def on_item(self, now: datetime, value: Any) -> Tuple[List[Any], bool]:
+        if self.item_triggers_notify:
+            self._notify_at = now
+
+        old_state = self._state
+        self._state = "ITEM"
+        return ([(old_state, self._state)], self.after_item)
+
+    def on_notify(self, sched: datetime) -> Tuple[List[Any], bool]:
+        old_state = self._state
+        self._state = "NOTIFY"
+        return ([(old_state, self._state)], self.after_notify)
+
+    def on_eof(self) -> Tuple[List[Any], bool]:
+        old_state = self._state
+        self._state = "EOF"
+        return ([(old_state, self._state)], self.after_eof)
+
+    def notify_at(self) -> Optional[datetime]:
+        return self._notify_at
+
+    def snapshot(self) -> Any:
+        return self._state
+
+
 def test_unary_on_item_discard():
-    inp = [1, 2]
+    inp = [1, 2, TestingSource.ABORT()]
     out = []
 
-    class TestLogic(UnaryLogic):
-        def __init__(self, _now: datetime, state: Any):
-            self._state = state if state is not None else "NEW"
-
-        def on_item(self, now: datetime, value: Any) -> Tuple[Iterable[Any], bool]:
-            old_state = self._state
-            self._state = "ITEM"
-            return ([(old_state, self._state)], UnaryLogic.DISCARD)
-
-        def on_notify(self, sched: datetime) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def on_eof(self) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def notify_at(self) -> Optional[datetime]:
-            return None
-
-        def snapshot(self) -> Any:
-            return self._state
+    class TestLogic(BaseTestLogic):
+        after_item = UnaryLogic.DISCARD
 
     flow = Dataflow("test_df")
     s = flow.input("inp", TestingSource(inp))
@@ -47,29 +64,11 @@ def test_unary_on_item_discard():
 
 
 def test_unary_on_item_retain():
-    inp = [1, 2]
+    inp = [1, 2, TestingSource.ABORT()]
     out = []
 
-    class TestLogic(UnaryLogic):
-        def __init__(self, _now: datetime, state: Any):
-            self._state = state if state is not None else "NEW"
-
-        def on_item(self, now: datetime, value: Any) -> Tuple[Iterable[Any], bool]:
-            old_state = self._state
-            self._state = "ITEM"
-            return ([(old_state, self._state)], UnaryLogic.RETAIN)
-
-        def on_notify(self, sched: datetime) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def on_eof(self) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def notify_at(self) -> Optional[datetime]:
-            return None
-
-        def snapshot(self) -> Any:
-            return self._state
+    class TestLogic(BaseTestLogic):
+        after_item = UnaryLogic.RETAIN
 
     flow = Dataflow("test_df")
     s = flow.input("inp", TestingSource(inp))
@@ -85,30 +84,12 @@ def test_unary_on_item_retain():
 
 
 def test_unary_on_notify_discard():
-    inp = [1, 2]
+    inp = [1, 2, TestingSource.ABORT()]
     out = []
 
-    class TestLogic(UnaryLogic):
-        def __init__(self, now: datetime, state: Any):
-            self._notify = now
-            self._state = state if state is not None else "NEW"
-
-        def on_item(self, now: datetime, value: Any) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def on_notify(self, sched: datetime) -> Tuple[Iterable[Any], bool]:
-            old_state = self._state
-            self._state = "NOTIFY"
-            return ([(old_state, self._state)], UnaryLogic.DISCARD)
-
-        def on_eof(self) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def notify_at(self) -> Optional[datetime]:
-            return self._notify
-
-        def snapshot(self) -> Any:
-            return self._state
+    class TestLogic(BaseTestLogic):
+        item_triggers_notify = True
+        after_notify = UnaryLogic.DISCARD
 
     flow = Dataflow("test_df")
     s = flow.input("inp", TestingSource(inp))
@@ -124,32 +105,12 @@ def test_unary_on_notify_discard():
 
 
 def test_unary_on_notify_retain():
-    inp = [1, 2]
+    inp = [1, 2, TestingSource.ABORT()]
     out = []
 
-    class TestLogic(UnaryLogic):
-        def __init__(self, now: datetime, state: Any):
-            self._notify: Optional[datetime] = now
-            self._state = state if state is not None else "NEW"
-
-        def on_item(self, now: datetime, value: Any) -> Tuple[Iterable[Any], bool]:
-            self._notify = now
-            return ([], UnaryLogic.RETAIN)
-
-        def on_notify(self, sched: datetime) -> Tuple[Iterable[Any], bool]:
-            old_state = self._state
-            self._state = "NOTIFY"
-            self._notify = None
-            return ([(old_state, self._state)], UnaryLogic.RETAIN)
-
-        def on_eof(self) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def notify_at(self) -> Optional[datetime]:
-            return self._notify
-
-        def snapshot(self) -> Any:
-            return self._state
+    class TestLogic(BaseTestLogic):
+        item_triggers_notify = True
+        after_notify = UnaryLogic.RETAIN
 
     flow = Dataflow("test_df")
     s = flow.input("inp", TestingSource(inp))
@@ -165,29 +126,11 @@ def test_unary_on_notify_retain():
 
 
 def test_unary_on_eof_discard(recovery_config):
-    inp = [1, TestingSource.EOF, 2]
+    inp = [1, TestingSource.EOF, 2, TestingSource.ABORT()]
     out = []
 
-    class TestLogic(UnaryLogic):
-        def __init__(self, _now: datetime, state: Any):
-            self._state = state if state is not None else "NEW"
-
-        def on_item(self, now: datetime, value: Any) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def on_notify(self, sched: datetime) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def on_eof(self) -> Tuple[Iterable[Any], bool]:
-            old_state = self._state
-            self._state = "EOF"
-            return ([(old_state, self._state)], UnaryLogic.DISCARD)
-
-        def notify_at(self) -> Optional[datetime]:
-            return None
-
-        def snapshot(self) -> Any:
-            return self._state
+    class TestLogic(BaseTestLogic):
+        after_eof = UnaryLogic.DISCARD
 
     flow = Dataflow("test_df")
     s = flow.input("inp", TestingSource(inp))
@@ -204,29 +147,11 @@ def test_unary_on_eof_discard(recovery_config):
 
 
 def test_unary_on_eof_retain(recovery_config):
-    inp = [1, TestingSource.EOF, 2]
+    inp = [1, TestingSource.EOF, 2, TestingSource.ABORT()]
     out = []
 
-    class TestLogic(UnaryLogic):
-        def __init__(self, _now: datetime, state: Any):
-            self._state = state if state is not None else "NEW"
-
-        def on_item(self, now: datetime, value: Any) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def on_notify(self, sched: datetime) -> Tuple[Iterable[Any], bool]:
-            return ([], UnaryLogic.RETAIN)
-
-        def on_eof(self) -> Tuple[Iterable[Any], bool]:
-            old_state = self._state
-            self._state = "EOF"
-            return ([(old_state, self._state)], UnaryLogic.RETAIN)
-
-        def notify_at(self) -> Optional[datetime]:
-            return None
-
-        def snapshot(self) -> Any:
-            return self._state
+    class TestLogic(BaseTestLogic):
+        after_eof = UnaryLogic.RETAIN
 
     flow = Dataflow("test_df")
     s = flow.input("inp", TestingSource(inp))
@@ -239,4 +164,5 @@ def test_unary_on_eof_retain(recovery_config):
 
     out.clear()
     run_main(flow, epoch_interval=ZERO_TD, recovery_config=recovery_config)
+    assert out == [("ALL", ("EOF", "EOF"))]
     assert out == [("ALL", ("EOF", "EOF"))]
