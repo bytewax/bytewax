d12caecb9072b22bcdbbf665119535c528eb66f8
David Selassie <dsel@fastmail.com>
2023-11-14 15:58:06 -0800

    Documentation about step names vs step ids

--- pysrc/bytewax/__init__.py
+++ pysrc/bytewax/__init__.py
@@ -209,22 +209,28 @@ can attach it on the end of any stream you'd like to see.
 
 TODO Example?
 
-### Step IDs
+### Step Names and Step IDs
 
-Adding descriptive step IDs to your steps is cruical because Bytewax's
-built-in operators will try to label thrown exceptions with the step
-they came from.
+Operator methods take a string step name as their first argument which
+should represent the semantic purpose of that computational step.
 
-Because operators are defined hierarchically into nested **substeps**,
-it's possible you'll see exceptions with step IDs that are dotted
-paths similar to `branching_math.do_double.flat_map`. Don't fret! Read
-them from left to right: the first component will be the flow ID, then
-your top-level step ID.
+Bytewax operators are defined in terms of other operators
+hierarchically in nested **substeps**. In order to disambiguate
+information about substeps under a single top-level step, each substep
+is assigned a unique **step ID**, which is a dotted path of all the
+step names down the heirarchy to that individual operator.
 
-Just because an exception references a substep of one of your step
-IDs, does not mean the bug is in the inner step. It's possible some
-logic function or value supplied is not conforming to the contract the
-operator set.
+This means exceptions, logs, traces, and metrics might show step IDs
+like `branching_math.do_double.flat_map`. Don't fret! Read them from
+left to right: the first component will be the flow ID, then the
+top-level step name, then the path to the specific inner substep the
+information applies to.
+
+Just because an exception references a step ID of a substep of one of
+your steps, does not mean the bug is in the inner step. It's possible
+some logic function or value supplied is not conforming to the
+contract the operator set. You'll need to read the exception and
+documentation of the operator carefully.
 
 ## Stateful Operators and Keyed Streams
 
