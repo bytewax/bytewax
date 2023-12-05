0b99152f469a41cd36cf0a54a70f9a84deec60e1
David Selassie <dsel@fastmail.com>
2023-12-05 10:29:49 -0800

    More realistic join example

--- examples/join.py
+++ examples/join.py
@@ -1,24 +1,27 @@
-from datetime import datetime, timedelta, timezone
-
 from bytewax import operators as op
 from bytewax.connectors.stdio import StdOutSink
 from bytewax.dataflow import Dataflow
-from bytewax.operators.window import SystemClockConfig, TumblingWindow
 from bytewax.testing import TestingSource
 
-clock = SystemClockConfig()
-windower = TumblingWindow(
-    length=timedelta(seconds=2), align_to=datetime(2023, 1, 1, tzinfo=timezone.utc)
+flow = Dataflow("join")
+
+
+def key_getter(x):
+    return str(x["user_id"])
+
+
+inp1 = op.input("inp1", flow, TestingSource([{"user_id": 123, "name": "Bumble"}]))
+(k_inp1,) = op.key_split("k1", inp1, key_getter, lambda x: x["name"])
+inp2 = op.input(
+    "inp2", flow, TestingSource([{"user_id": 123, "email": "bee@bytewax.com"}])
+)
+(k_inp2,) = op.key_split("k2", inp2, key_getter, lambda x: x["email"])
+inp3 = op.input(
+    "inp3", flow, TestingSource([{"user_id": 123, "color": "yellow", "sound": "buzz"}])
+)
+k_inp3, k_inp4 = op.key_split(
+    "k3", inp3, key_getter, lambda x: x["color"], lambda x: x["sound"]
 )
 
-
-flow = Dataflow("join")
-inp1 = op.input("inp1", flow, TestingSource(["a"]))
-k_inp1 = op.key_on("k1", inp1, lambda x: "KEY")
-inp2 = op.input("inp2", flow, TestingSource(["b"]))
-k_inp2 = op.key_on("k2", inp2, lambda x: "KEY")
-inp3 = op.input("inp3", flow, TestingSource(["c"]))
-k_inp3 = op.key_on("k3", inp3, lambda x: "KEY")
-
-joined = op.join("j1", k_inp1, k_inp2, k_inp3)
+joined = op.join("j1", k_inp1, k_inp2, k_inp3, k_inp4)
 op.output("out", joined, StdOutSink())
