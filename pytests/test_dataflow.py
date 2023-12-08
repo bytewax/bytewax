999b8c805185c21b77cfec52d94fde8148c0942a
David Selassie <dsel@fastmail.com>
2023-12-08 14:54:58 -0800

    Runtime typecheck `Stream` arguments to operators
    If you don't have an LSP turned on, a common mistake is to forget to
    "unpack" an operator that returns multiple streams. E.g. you
    accidentally pass an entire `BranchOut` in where a `Stream` should go
    instead of `BranchOut.trues`.
    
    This usually manifests as a strange exception when the random type is
    attempted to be scoped, and you get `BranchOut._scope` doesn't exist.
    This isn't clear what's going on, so adds in some proactive type
    checking when you call each operator so that each upstream is truly a
    `Stream`.
    
    This means we don't need to duplicate the "you forgot to unpack this"
    message on every multiple return value.
    
    Also gets rid of the `_AsArgs` protocol; we only used it for `Stream`
    anyway and so made this more confusing then it needed to be.
    
    Adds tests.

--- pytests/test_dataflow.py
+++ pytests/test_dataflow.py
@@ -20,7 +20,7 @@ def test_raises_on_nested_stream():
     inp2 = op.input("inp2", flow, TestingSource([]))
 
     expect = "inconsistent stream scoping"
-    with raises(ValueError, match=re.escape(expect)):
+    with raises(AssertionError, match=re.escape(expect)):
         bad_op_with_nested_stream("bad", inp1, [inp2])
 
 
@@ -40,3 +40,37 @@ def test_then():
 
     run_main(flow)
     assert out == [1, 2, 3]
+
+
+def test_step_id_check_str():
+    flow = Dataflow("test_df")
+
+    expect = "must be a `str`"
+    with raises(TypeError, match=re.escape(expect)):
+        op.input(1, flow, TestingSource([]))  # type: ignore
+
+
+def test_step_id_check_periods():
+    flow = Dataflow("test_df")
+
+    expect = "can't contain any periods"
+    with raises(ValueError, match=re.escape(expect)):
+        op.input("1.5", flow, TestingSource([]))
+
+
+def test_check_non_stream():
+    expect = "must be a `Stream`"
+    with raises(TypeError, match=re.escape(expect)):
+        op.map("map", 1, lambda x: x)  # type: ignore
+
+
+def test_check_non_stream_vararg():
+    expect = "must be a `Stream`"
+    with raises(TypeError, match=re.escape(expect)):
+        op.merge("map", 1, 2, 3)  # type: ignore
+
+
+def test_check_non_stream_varkwarg():
+    expect = "must be a `Stream`"
+    with raises(TypeError, match=re.escape(expect)):
+        op.join_named("map", side_a=1, side_b=2)  # type: ignore
