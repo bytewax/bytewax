c55c8148a229c3f750af64bae07bb5393c2f02f7
Dan Herrera <whoahbot@gmail.com>
2024-01-11 14:25:33 -0800

    Revert changes to the simple kafka example

--- examples/simple_kafka_in_and_out.py
+++ examples/simple_kafka_in_and_out.py
@@ -1,5 +1,4 @@
 from bytewax import operators as op
-from bytewax.connectors.kafka import KafkaSinkMessage
 from bytewax.connectors.kafka import operators as kop
 from bytewax.dataflow import Dataflow
 
@@ -11,5 +10,4 @@ flow = Dataflow("kafka_in_out")
 kinp = kop.input("inp", flow, brokers=BROKERS, topics=IN_TOPICS)
 op.inspect("inspect-errors", kinp.errs)
 op.inspect("inspect-oks", kinp.oks)
-processed = op.map("map", kinp.oks, lambda x: KafkaSinkMessage(x.key, x.value))
-kop.output("out1", processed, brokers=BROKERS, topic=OUT_TOPIC)
+kop.output("out1", kinp.oks, brokers=BROKERS, topic=OUT_TOPIC)
