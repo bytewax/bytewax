c61f8ecb792dc90707c0367ed827a06dc2b64cf7
David Selassie <dsel@fastmail.com>
2024-02-02 13:41:45 -0800

    `pytest` configuration to enable Sybil

--- conftest.py
+++ conftest.py
@@ -1,78 +1,34 @@
-"""Custom pytest config for `pytests/`."""
-from datetime import datetime, timezone
+"""Project-wide `pytest` config.
 
-from bytewax.recovery import RecoveryConfig, init_db_dir
-from bytewax.testing import cluster_main, run_main
-from bytewax.tracing import setup_tracing
-from pytest import fixture
+This sets up our documentation test config.
 
+See the [documentation for
+Sybil](https://sybil.readthedocs.io/en/latest/index.html) for details
+here.
 
-def pytest_addoption(parser):
-    """Add a `--bytewax-log-level` CLI option to pytest.
+This config tells Sybil to read the _Python source files_ and look for
+Markdown code blocks and attempt to parse them as docstrings. This
+means it does not dynamically look at the docstrings of the installed
+version of Bytewax, but just the source.
 
-    This will control the `setup_tracing` log level.
+Docstrings from PyO3 will be checked via the `_bytewax.pyi` stubs
+file.
 
-    """
-    parser.addoption(
-        "--bytewax-log-level",
-        action="store",
-        choices=["ERROR", "WARN", "INFO", "DEBUG", "TRACE"],
-    )
+See the "Writing Docs" article about all the ways to do that and
+configure behavior from the code block itself.
 
+"""
+import doctest
 
-def pytest_configure(config):
-    """This will run on pytest init."""
-    log_level = config.getoption("--bytewax-log-level")
-    if log_level:
-        setup_tracing(log_level=log_level)
+from sybil import Sybil
+from sybil.parsers import myst
 
+doctest_option_flags = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
 
-@fixture(params=["run_main", "cluster_main-2thread"])
-def entry_point_name(request):
-    """Run a version of the test for each execution point.
-
-    You probably want to use the `entry_point` fixture to get a
-    callable instead of the name here.
-
-    There will be `"run_main"` for single in-thread, and
-    `"cluster_main-2thread"` for launching 2 worker sub-threads.
-
-    """
-    return request.param
-
-
-def _wrapped_cluster_main1x2(*args, **kwargs):
-    return cluster_main(*args, [], 0, worker_count_per_proc=2, **kwargs)
-
-
-@fixture
-def entry_point(entry_point_name):
-    """Run a version of this test for each execution point.
-
-    See `entry_point_name` for options.
-
-    """
-    if entry_point_name == "run_main":
-        return run_main
-    elif entry_point_name == "cluster_main-2thread":
-        return _wrapped_cluster_main1x2
-    else:
-        msg = "unknown entry point name: {request.param!r}"
-        raise ValueError(msg)
-
-
-@fixture
-def recovery_config(tmp_path):
-    """Generate a recovery config.
-
-    It will point to a single partition recovery store.
-
-    """
-    init_db_dir(tmp_path, 1)
-    yield RecoveryConfig(str(tmp_path))
-
-
-@fixture
-def now():
-    """Get the current `datetime` in UTC."""
-    yield datetime.now(timezone.utc)
+pytest_collect_file = Sybil(
+    parsers=[
+        myst.PythonCodeBlockParser(doctest_optionflags=doctest_option_flags),
+        myst.SkipParser(),
+    ],
+    patterns=["*.md", "*.py", "*.pyi"],
+).pytest()
