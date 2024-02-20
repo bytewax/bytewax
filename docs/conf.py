c16f7ab22fd7cf6de964bd3b37f22dbe1a71b505
Konrad Sienkowski <konrad@sienkowski.eu>
2024-02-20 22:21:11 +0100

    [Docs]: Disable GA integration, disable "Edit this page" button on API reference

--- docs/conf.py
+++ docs/conf.py
@@ -127,10 +127,9 @@ html_theme_options = {
     # On the per-page right hand side TOC, show more depth by default.
     "show_toc_level": 3,
     "show_prev_next": True,
-    # Set Google Analytics integration
-    # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/analytics.html
-    "analytics": {
-        "google_analytics_id": "G-06HTJPTC44",
+    "secondary_sidebar_items": {
+        "apidocs/**": ["page-toc"],
+        "articles/**": ["page-toc", "edit-this-page"],
     },
 }
 
