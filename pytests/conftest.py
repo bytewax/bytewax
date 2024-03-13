797cff50bcfabdc7222070e2ae4f75670f120d84
Dan Herrera <whoahbot@gmail.com>
2024-03-13 15:41:22 -0700

    Respond to PR feedback
    - Removes BatchSource testing input.
    - Merges benchmark tests with operator tests.
    - Adds an additional cluster_main entrypoint with one worker to
      `entry_point` fixture.
    - Adds asserts to benchmark tests.
    - Removes merge operator from branch benchmark test.

--- pytests/conftest.py
+++ pytests/conftest.py
@@ -28,6 +28,11 @@ def _wrapped_cluster_main1x2(*args, **kwargs):
     return cluster_main(*args, [], 0, worker_count_per_proc=2, **kwargs)
 
 
+def _wrapped_cluster_main1x1(*args, **kwargs):
+    """Used in benchmarking tests."""
+    return cluster_main(*args, [], 0, **kwargs)
+
+
 @fixture
 def entry_point(entry_point_name):
     """Run a version of this test for each execution point.
@@ -37,6 +42,8 @@ def entry_point(entry_point_name):
     """
     if entry_point_name == "run_main":
         return run_main
+    elif entry_point_name == "cluster_main_one_worker":
+        return _wrapped_cluster_main1x1
     elif entry_point_name == "cluster_main-2thread":
         return _wrapped_cluster_main1x2
     else:
--- pytests/operators/test_flat_map_batch.py
+++ pytests/operators/test_flat_map_batch.py
@@ -3,6 +3,7 @@ from typing import Iterable, List
 import bytewax.operators as op
 from bytewax.dataflow import Dataflow
 from bytewax.testing import TestingSink, TestingSource, run_main
+from pytest import mark
 
 
 def test_flat_map_batch():
@@ -20,3 +21,26 @@ def test_flat_map_batch():
 
     run_main(flow)
     assert out == ["split", "this", "and", "this"]
+
+
+def build_flat_map_batch_dataflow(out: List) -> Dataflow:
+    flow = Dataflow("flat_map_batch")
+    inp = TestingSource(range(100_000), 10)
+    s = op.input("inp", flow, inp)
+    batch_out = op.flat_map_batch("flat_map", s, lambda xs: (x for x in xs))
+    op.output("out", batch_out, TestingSink(out))
+    return flow
+
+
+def run_flat_map_batch_dataflow(entry_point, flow, out, expected):
+    entry_point(flow)
+    assert out == expected
+    out.clear()
+
+
+@mark.parametrize("entry_point_name", ["run_main", "cluster_main_one_worker"])
+def test_flat_map_batch_benchmark(benchmark, entry_point):
+    out = []
+    flow = build_flat_map_batch_dataflow(out)
+    expected = list(range(100_000))
+    benchmark(lambda: run_flat_map_batch_dataflow(entry_point, flow, out, expected))
