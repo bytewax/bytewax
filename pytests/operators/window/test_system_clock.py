4df3b5fdf0c31a51ae9c65fb655e2a58c2ce90c8
David Selassie <dsel@fastmail.com>
2024-04-04 09:48:09 -0700

    Adds system clock test

---
+++ pytests/operators/window/test_system_clock.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timezone
+
+from bytewax.operators.window import UTC_MAX, _SystemClockLogic
+from bytewax.testing import TimeTestingGetter
+
+
+def test_watermark_is_end_of_time_on_eof():
+    source = TimeTestingGetter(datetime(2024, 1, 1, tzinfo=timezone.utc))
+
+    logic = _SystemClockLogic(source.get)
+    logic.on_eof()
+    assert logic.on_eof() == UTC_MAX
