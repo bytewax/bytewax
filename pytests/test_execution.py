c8ae7396466fcbc753a982c4c6bf824eaf8bf5b3
David Selassie <dsel@fastmail.com>
2024-04-30 11:03:44 -0700

    Tests ctrl-c of all entry points

--- pytests/test_execution.py
+++ pytests/test_execution.py
@@ -3,6 +3,7 @@ import signal
 import subprocess
 import sys
 from datetime import datetime, timedelta, timezone
+from typing import BinaryIO
 
 import bytewax.operators as op
 from bytewax.dataflow import Dataflow
@@ -45,6 +46,51 @@ def test_reraises_exception(entry_point):
     assert len(out) < 3
 
 
+def _assert_can_be_ctrl_c(proc: subprocess.Popen, out_file: BinaryIO):
+    try:
+        # Wait for the file to contain at least a line to show the
+        # dataflow has started.
+        output = b""
+        timeout_at = datetime.now(tz=timezone.utc) + timedelta(seconds=5)
+        while len(output.splitlines()) < 1:
+            if datetime.now(tz=timezone.utc) >= timeout_at:
+                msg = "dataflow didn't write output in time"
+                raise TimeoutError(msg)
+            proc.poll()
+            if proc.returncode is not None:
+                msg = "dataflow exited too quickly"
+                raise RuntimeError(msg)
+
+            out_file.seek(0)
+            output = out_file.read()
+
+        # And stop the dataflow by sending SIGINT (like ctrl+c)
+        proc.send_signal(signal.SIGINT)
+
+        # Process termination should be handled properly
+        stdout, stderr = proc.communicate(timeout=5)
+        assert b"KeyboardInterrupt" in stderr
+
+        # The file should not contain all the lines since we stopped it
+        out_file.seek(0)
+        output = out_file.read()
+        assert len(output.splitlines()) < 999
+    except (subprocess.TimeoutExpired, TimeoutError, RuntimeError) as ex:
+        proc.kill()
+        stdout, stderr = proc.communicate()
+        print("--- Captured STDOUT of subprocess ---")
+        sys.stdout.buffer.write(stdout)
+        print("--- Captured STDERR of subprocess ---")
+        sys.stdout.buffer.write(stderr)
+        print("-------------------------------------")
+        raise subprocess.CalledProcessError(
+            proc.returncode,
+            proc.args,
+            stdout,
+            stderr,
+        ) from ex
+
+
 @mark.skipif(
     os.name == "nt",
     reason=(
@@ -52,14 +98,91 @@ def test_reraises_exception(entry_point):
         " processes on this console so interrupts pytest itself"
     ),
 )
-def test_cluster_can_be_ctrl_c(tmp_path):
+def test_single_worker_can_be_ctrl_c(tmp_path):
+    tmp_path = tmp_path / "out.txt"
+
+    with open(tmp_path, "w+b") as tmp_file:
+        # The dataflow we want to run is in ./test_flows/simple.py
+        flow_path = f"pytests.test_flows.simple:get_flow('{tmp_file.name}')"
+        args = [
+            # Ensure that we use the exact same Python interpreter as
+            # here; might be in a venv.
+            sys.executable,
+            "-m",
+            "bytewax.run",
+            flow_path,
+            # With 1 worker per process to ensure `run_main` is used.
+            "-w",
+            "1",
+            # Set snapshot interval to 0 so that the output is written
+            # to the file as soon as possible
+            "-s",
+            "0",
+        ]
+        proc = subprocess.Popen(
+            args,
+            # Use PIPE to check the content of stdout
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        _assert_can_be_ctrl_c(proc, tmp_file)
+
+
+@mark.skipif(
+    os.name == "nt",
+    reason=(
+        "Sending os.kill(test_proc.pid, signal.CTRL_C_EVENT) sends event to all"
+        " processes on this console so interrupts pytest itself"
+    ),
+)
+def test_manual_cluster_can_be_ctrl_c(tmp_path):
+    tmp_path = tmp_path / "out.txt"
+
+    with open(tmp_path, "w+b") as tmp_file:
+        # The dataflow we want to run is in ./test_flows/simple.py
+        flow_path = f"pytests.test_flows.simple:get_flow('{tmp_file.name}')"
+        args = [
+            # Ensure that we use the exact same Python interpreter as
+            # here; might be in a venv.
+            sys.executable,
+            "-m",
+            "bytewax.run",
+            flow_path,
+            # With 2 worker per process to ensure `cluster_main` is
+            # used.
+            "-w",
+            "2",
+            # Set snapshot interval to 0 so that the output is written
+            # to the file as soon as possible
+            "-s",
+            "0",
+        ]
+        proc = subprocess.Popen(
+            args,
+            # Use PIPE to check the content of stdout
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        _assert_can_be_ctrl_c(proc, tmp_file)
+
+
+@mark.skipif(
+    os.name == "nt",
+    reason=(
+        "Sending os.kill(test_proc.pid, signal.CTRL_C_EVENT) sends event to all"
+        " processes on this console so interrupts pytest itself"
+    ),
+)
+def test_testing_cluster_can_be_ctrl_c(tmp_path):
     """Test that we can stop cluster execution by sending SIGINT (ctrl+c)."""
     tmp_path = tmp_path / "out.txt"
 
     with open(tmp_path, "w+b") as tmp_file:
         # The dataflow we want to run is in ./test_flows/simple.py
         flow_path = f"pytests.test_flows.simple:get_flow('{tmp_file.name}')"
-        cmd = [
+        args = [
             # Ensure that we use the exact same Python interpreter as
             # here; might be in a venv.
             sys.executable,
@@ -77,52 +200,11 @@ def test_cluster_can_be_ctrl_c(tmp_path):
             "-s",
             "0",
         ]
-        process = subprocess.Popen(
-            cmd,
+        proc = subprocess.Popen(
+            args,
             # Use PIPE to check the content of stdout
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
 
-        try:
-            # Wait for the file to contain at least a line to show the
-            # dataflow has started.
-            output = b""
-            timeout_at = datetime.now(tz=timezone.utc) + timedelta(seconds=5)
-            while len(output.splitlines()) < 1:
-                if datetime.now(tz=timezone.utc) >= timeout_at:
-                    msg = "dataflow didn't write output in time"
-                    raise TimeoutError(msg)
-                process.poll()
-                if process.returncode is not None:
-                    msg = "dataflow exited too quickly"
-                    raise RuntimeError(msg)
-
-                tmp_file.seek(0)
-                output = tmp_file.read()
-
-            # And stop the dataflow by sending SIGINT (like ctrl+c)
-            process.send_signal(signal.SIGINT)
-
-            # Process termination should be handled properly
-            stdout, stderr = process.communicate(timeout=5)
-            assert b"KeyboardInterrupt:" in stderr
-
-            # The file should not contain all the lines since we stopped it
-            tmp_file.seek(0)
-            output = tmp_file.read()
-            assert len(output.splitlines()) < 999
-        except (subprocess.TimeoutExpired, TimeoutError, RuntimeError) as ex:
-            process.kill()
-            stdout, stderr = process.communicate()
-            print("--- Captured STDOUT of subprocess ---")
-            sys.stdout.buffer.write(stdout)
-            print("--- Captured STDERR of subprocess ---")
-            sys.stdout.buffer.write(stderr)
-            print("-------------------------------------")
-            raise subprocess.CalledProcessError(
-                process.returncode,
-                cmd,
-                stdout,
-                stderr,
-            ) from ex
+        _assert_can_be_ctrl_c(proc, tmp_file)
